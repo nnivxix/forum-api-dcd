2ea45d53fb69aba89babc42ed157f3f459e9d37b
nnivxix <adhe.hanasa@gmail.com>
2023-08-27 20:29:47 +0700

    feat: add comment repository

---
+++ src/Domains/comments/_test/CommentRepository.test.js
@@ -0,0 +1,16 @@
+const CommentRepository = require("../CommentRepository");
+
+describe("CommentRepository interface", () => {
+  it("should throw error when invoke abstrack behavior", async () => {
+    const commentRepository = new CommentRepository();
+    await expect(commentRepository.addComment({})).rejects.toThrowError(
+      "COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED"
+    );
+    await expect(commentRepository.verifyCommentExist({})).rejects.toThrowError(
+      "COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED"
+    );
+    await expect(
+      commentRepository.getCommentByThreadId({})
+    ).rejects.toThrowError("COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  });
+});
